8350c0b7eefe6f5a72264dc80f8def5c43f403e6
splendiferouswar <47614099+splendiferouswar@users.noreply.github.com>
2021-05-21 12:26:28 +1200

    Made a cool as intro to the game (work in progress)

---
+++ Scripts/IntroDialogueScript.gd
@@ -0,0 +1,31 @@
+extends Panel
+
+var dialog = ["In the super cluster RUST9102_HRMAEP"]
+var dialog_index = 0
+var finished = false
+
+func _ready():
+	load_dialog()
+
+func _process(delta):
+	#$grey_button04.visible = finished
+	#if Input.is_action_just_pressed("ui_accept"):
+	load_dialog()		
+		
+func load_dialog():
+	if dialog_index < dialog.size():
+		finished = false
+		$IntroDialogueScript.bbcode_text = dialog[dialog_index]
+		$IntroDialogueScript.percent_visible = 0
+		$Tween.interpolate_property(
+			$IntroDialogueScript, "percent_visible", 0,1,1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT
+		)
+		$Tween.start()
+	else:
+		get_tree().change_scene("res://Scene/Level1.tscn")
+	dialog_index += 1
+
+
+
+func _on_Tween_tween_completed(object, key):
+	finished = true # Replace with function body.
