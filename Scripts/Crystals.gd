647147fcf8a3edc346818a7d396d2dfceb309c80
splendiferouswar <47614099+splendiferouswar@users.noreply.github.com>
2021-05-23 10:55:28 +1200

    Parallax Background added

--- Scripts/Crystals.gd
+++ Scripts/Crystals.gd
@@ -1,27 +1,15 @@
 extends Area2D
 
-onready var stamina = get_node("/root/Cave/MainHUD").get_node("CanvasLayer/Control/Stamina_Bar_Script")
-onready var health = get_node("/root/Cave/MainHUD").get_node("CanvasLayer/Control/Health_Bar_Script")
+onready var stamina = get_node("/root/Level/MainHUD").get_node("CanvasLayer/Control/Stamina_Bar_Script")
+onready var health = get_node("/root/Level/MainHUD").get_node("CanvasLayer/Control/Health_Bar_Script")
 onready var global_vars = get_node("/root/Globals")
 
 export var mode = "Health"
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Rest")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
+	pass 
+	
 func _on_Crystal_body_entered(body):
 	if body.is_in_group("Player") || body.is_in_group("Fireball"):
 		if mode == "Health":
---
+++ Scripts/ParallaxBackground.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+
+func _ready():
+	#$ParallaxBackground/ParallaxLayer3/AnimationPlayer.play("TwinklingStars")
+	#$ParallaxBackground/ParallaxLayer4/AnimationPlayer.play("TwinklingStars")
+	#$ParallaxBackground/ParallaxLayer2/AnimationPlayer.play("TwinklingStars")
+	pass
