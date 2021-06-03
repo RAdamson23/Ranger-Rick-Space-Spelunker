76511a688cd9d1149676f2e342fc2c080c6016bb
Robert Adamson <rwdadamson@gmail.com>
2021-06-03 22:24:58 +1200

    Added some more detail to CaveInterior1, also added the portal animated sprite

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _ready():
 	pass # Replace with function body.
-
+export var nextLevel = "CaveInterior1.tscn"
 func _on_Portal_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene("res://Levels/CaveInterior1.tscn")
+		get_tree().change_scene("res://Levels/"+nextLevel)
